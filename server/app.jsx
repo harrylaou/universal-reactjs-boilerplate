124e2d33412d1d2aa1442d981e42158ecfe60edb
Alex Lobera <alex.lobera@gmail.com>
2017-04-08 18:21:36 +0100

    Update app.jsx

--- server/app.jsx
+++ server/app.jsx
@@ -3,7 +3,7 @@ import { RouterContext, match } from 'react-router';
 import routes from '../shared/routes';
 
 const reactApp = (req, res) => {
-  // Tip: https://github.com/reactjs/react-router/blob/master/docs/guides/ServerRendering.md
+  // Tip: https://github.com/reactjs/react-router-tutorial/tree/master/lessons/13-server-rendering
   match({ routes, location: req.url }, (error, redirectLocation, renderProps) => {
     if (error) {
       res.status(500).send(error.message)
